e4b4e5b6ca5917759692ff9ea608a1c990dbd467
Орлова Анастасия Владимировна <@itstep.loc>
2023-03-02 21:29:42 +0600

    lesson_03: files

--- stepshop/stepshop/views.py
+++ stepshop/stepshop/views.py
@@ -1,7 +1,41 @@
 from django.shortcuts import render
 
+linkы_menu = [
+        {'href': 'index', 'name': 'Главная', 'route': ''},
+        {'href': 'products:index', 'name': 'Продукты', 'route': 'products/'},
+        {'href': 'about', 'name': 'О нас', 'route': 'about/'},
+        {'href': 'contacts', 'name': 'Контакты', 'route': 'contacts/'},
+    ]
+
 def index(request):
-    return render(request, 'index.html')
+    title = "главная"
+
+    context = {
+        'title': title,
+        'links_menu': linkы_menu,
+    }
+
+    return render(request, 'index.html', context)
 
 def contacts(request):
     return render(request, 'contacts.html')
+
+def contacts(request):
+    title = "контакты"
+
+    context = {
+        'title': title,
+        'links_menu': linkы_menu,
+    }
+
+    return render(request, 'contacts.html', context)
+
+def about(request):
+    title = "о нас"
+
+    context = {
+        'title': title,
+        'links_menu': linkы_menu,
+    }
+
+    return render(request, 'about.html', context)
