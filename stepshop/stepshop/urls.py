45b9de666a57871b8592f223d0941d0e40aa44ae
Орлова Анастасия Владимировна <@itstep.loc>
2023-03-07 21:35:14 +0600

    lesson_04 files

--- stepshop/stepshop/urls.py
+++ stepshop/stepshop/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from stepshop import settings
 from stepshop.views import index, contacts, about
 
 urlpatterns = [
@@ -25,3 +27,6 @@ urlpatterns = [
     path('about/', about, name='about'),
     path('products/', include('mainapp.urls', namespace='products')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
