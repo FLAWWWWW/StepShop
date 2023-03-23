75e925c355b9e89eb253d6925aac9423828ffa44
Oyasumi <95757737+FLAWWWWW@users.noreply.github.com>
2023-03-23 12:28:04 +0600

    homework_04

--- stepshop/mainapp/views.py
+++ stepshop/mainapp/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 
+linkы_menu = [
+        {'href': 'index', 'name': 'Главная', 'route': ''},
+        {'href': 'products:index', 'name': 'Продукты', 'route': 'products/'},
+        {'href': 'about', 'name': 'О нас', 'route': 'about/'},
+        {'href': 'contacts', 'name': 'Контакты', 'route': 'contacts/'},
+    ]
 
 def products(request):
     title = "продукты/каталог"
 
     context = {
         'title': title,
+        'links_menu': linkы_menu,
     }
 
     return render(request, 'products.html', context)
