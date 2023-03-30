c1e82e68105c770dd38fb96f76a04b3606066621
Орлова Анастасия Владимировна <@itstep.loc>
2023-03-30 21:45:38 +0600

    'lesson_09'

--- stepshop/mainapp/views.py
+++ stepshop/mainapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from basketapp.models import Basket
 from mainapp.models import ProductCategory, Product
 
 linkы_menu = [
@@ -13,15 +14,40 @@ linkы_menu = [
 def products(request):
     title = "продукты/каталог"
 
-    products = Product.objects.all()[:2]
+    products = Product.objects.all().order_by('-price') #[:2]
     categories = ProductCategory.objects.all()
+
+    basket = []
+
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
 
     context = {
         'title': title,
         'links_menu': linkы_menu,
         'products': products,
         'categories': categories,
+        'basket': basket,
     }
 
     return render(request, 'products.html', context)
     # return render(request = request, template_name='products.html', context=context)
+
+def product(request, pk):
+    title = "продукт"
+
+    product_item = get_object_or_404(Product, pk=pk)
+    category = product_item.category
+    products = Product.objects.all().order_by('-price')  # [:2]
+    categories = ProductCategory.objects.all()
+
+    context = {
+        'title': title,
+        'links_menu': linkы_menu,
+        'product': product_item,
+        'category': category,
+        'products': products,
+        'categories': categories,
+    }
+
+    return render(request, 'product.html', context)
