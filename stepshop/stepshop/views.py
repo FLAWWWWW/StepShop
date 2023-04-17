31ee5d49ae45490d7d7b806700c6740f5c8d01eb
Oyasumi <95757737+FLAWWWWW@users.noreply.github.com>
2023-04-17 14:53:34 +0600

    homework

--- stepshop/stepshop/views.py
+++ stepshop/stepshop/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from mainapp.models import ProductCategory, Product
 
+from basketapp.models import Basket
+
 linkы_menu = [
         {'href': 'index', 'name': 'Главная', 'route': ''},
         {'href': 'products:index', 'name': 'Продукты', 'route': 'products/'},
@@ -15,24 +17,32 @@ def index(request):
     products = Product.objects.all()[:2]
     categories = ProductCategory.objects.all()
 
+    basket = []
+
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+
     context = {
         'title': title,
         'links_menu': linkы_menu,
         'products': products,
         'categories': categories,
+        'basket': basket,
     }
 
     return render(request, 'index.html', context)
 
 def contacts(request):
-    return render(request, 'contacts.html')
-
-def contacts(request):
     title = "контакты"
+    basket = []
+
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
 
     context = {
         'title': title,
         'links_menu': linkы_menu,
+        'basket': basket,
     }
 
     return render(request, 'contacts.html', context)
@@ -40,9 +50,14 @@ def contacts(request):
 def about(request):
     title = "о нас"
 
+    basket = []
+
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
     context = {
         'title': title,
         'links_menu': linkы_menu,
+        'basket': basket,
     }
 
     return render(request, 'about.html', context)
